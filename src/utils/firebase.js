9415bf3e3119eb2b3cb29a37bb3f7ba9b53cf647
Lucas Dias <100321484+LucasDiasAl@users.noreply.github.com>
2024-08-12 15:36:40 -0300

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCixX1Md_GPFpCeZScrdi_Kj4uL64JuYWQ',
+  apiKey: '',
   authDomain: 'recipeapp-e9dbe.firebaseapp.com',
   projectId: 'recipeapp-e9dbe',
   storageBucket: 'recipeapp-e9dbe.appspot.com',
